971e2e9cd5752cce09852560a0f155334abc63e7
ShamithaParuchuru <shamitha.0123@gmail.com>
2026-03-10 11:11:04 +0530

    stepfunction.tf

--- stepfunction.tf
+++ stepfunction.tf
@@ -1,5 +1,5 @@
 resource "aws_sfn_state_machine" "workflow" {
-  name     = "abc-workflow-v3"
+  name     = "abc-workflow-v3-demo"
   role_arn = aws_iam_role.step_role.arn
  
   definition = jsonencode({
